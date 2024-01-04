45b24bc9c439c5b5148b3b7dd0788d02b0f2953b
librossard <librossard@etudiant.univ-rennes1.fr>
2024-01-04 19:26:52 +0100

    config : test2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/LilianBrossard/portfolio',
+    site: 'https://LilianBrossard.github.io',
 });
